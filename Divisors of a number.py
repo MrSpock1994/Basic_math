1d6c028c7d5ac65c9f976347f524a74e1347d8a8
MrSpock1994 <willpokerpro2016@gmail.com>
2021-07-30 23:35:26 -0300

    First commit

---
+++ Divisors of a number.py
@@ -0,0 +1,22 @@
+"""
+Program to inform the user all the divisors of a certain number.
+
+"""
+
+divisors = []
+
+while True:
+    num = int(input('What is the number? '))
+    for c in range(1,num+1):
+        if num % c == 0:
+            divisors.append(c)
+    print(f'The divisors of {num} are {divisors}.')
+    print()
+    another = str(input('Do you want to input another number? [Y/N]: ')).upper()
+    print()
+    while another not in 'YN':
+        print('Invalid choice!')
+        another = str(input('Do you want to input another number? [Y/N]')).upper()
+    if another == 'N':
+        print('END!')
+        break
