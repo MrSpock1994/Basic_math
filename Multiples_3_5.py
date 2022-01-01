339d43db9341883d03f541a9f595a38daa77e0f1
MrSpock1994 <willpokerpro2016@gmail.com>
2022-01-01 11:07:09 +0100

    First commit

---
+++ Multiples_3_5.py
@@ -0,0 +1,21 @@
+"""
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+"""
+
+
+def soma(a, b):
+    n = 1
+    multiples = []
+    while n < 1000:
+        if n % a == 0 or n % b == 0:
+            multiples.append(n)
+        n += 1
+    print(f'The sum is {sum(multiples)}')
+
+
+soma(3, 5)
+
